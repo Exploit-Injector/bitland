00ebd80b327e5d44df69c46bbb1a85b384faf816
Feedbug77 <55884881+Exploit-Injector@users.noreply.github.com>
2020-04-09 14:04:58 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,15 @@
+<?php
+
+// MASUKAN DATA COOKIE KALIAN
+// ENTER YOUR DATA COOKIE
+
+$__cfduid = "";
+
+$__tawkuuid = "";
+
+// MASUKAN DATA LOGIN KALIAN
+// ENTER YOUR DATA LOGIN
+
+$email = "";
+
+$password = "";
