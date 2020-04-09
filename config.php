7ae30722fe7c18942e8339cef564b78d49359f68
Feedbug77 <55884881+Exploit-Injector@users.noreply.github.com>
2020-04-09 14:44:42 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?
+// Ambil User Agent dari config Script indigocoin atau mastercoin.
+// Buat yang belum pernah daftar. Capture dulu ambil User Agentnya agar tidak system tidak mendetek penyalahgunaan
+
+$UserAgent = "xxxxxxxxx";
+$Email = "xxxxxxxx@gmail.com";
+$Password = "xxxxxxxxx";
